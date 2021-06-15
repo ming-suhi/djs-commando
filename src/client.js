fc4139f6fa51f400447acb4301959ab87809ba45
明suhi <61369062+ming-suhi@users.noreply.github.com>
2021-06-15 17:26:44 +0800

    Add matchCommand method Client class
    This method finds requested command. It then check if the user has permissions to use command, it executes command on pass, and sends an ephemeral message on fail.

--- src/client.js
+++ src/client.js
@@ -5,10 +5,32 @@ class Client {
 
   /**
    * Client for managing local
+   * @property {CommandsManager} commands manage commands
+   * @property {string} token bot token
    */
   constructor() {
     dotenv.config();
     this.commands = new CommandsManager(process.env.COMMANDS_FOLDER);
+    this.token = process.env.BOT_TOKEN;
+  }
+
+
+  /**
+   * Finds requested command
+   * @param {Interaction} interaction interaction 
+   */
+  async matchCommand(interaction){
+    const command = await this.commands.get(interaction.request.data.name);
+    const security = command.security(await interaction.author());
+    switch (security.pass) {
+      case true:
+      command.execute(interaction);
+      break;
+    
+      case false:
+      interaction.responseType = 3;
+      interaction.sendEphemeral(`You are missing permissions to run this command: \`${security.missingPermissions.join(' | ').replace(/_/g, ' ')}\``);
+    }
   }
 }
 
