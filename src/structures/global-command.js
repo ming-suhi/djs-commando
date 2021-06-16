887faf6f802acd4adc353d3e20ab399897067414
明suhi <61369062+ming-suhi@users.noreply.github.com>
2021-06-16 20:22:43 +0800

    Add GlobalCommands class methods
    Added get, post, delete methods. All of which are methods to coordinate with Discord Client.

--- src/structures/global-command.js
+++ src/structures/global-command.js
@@ -12,6 +12,46 @@ class GlobalCommand extends Command{
     super();
     Object.assign(this, data);
   }
+
+
+  /**
+   * Initializes local command structure
+   * @param {Discord.Client} client discord client
+   * @returns {this} this instance
+   */
+  async get(client) {
+    const commands = await client.api.applications(client.user.id).commands.get();
+    const command = commands.find(command => command.name === this.name);
+    this.id = command.id;
+    return this;
+  }
+
+
+  /**
+   * Posts command to Discord
+   * @param {Discord.Client} client discord client
+   * @returns {this} this instance
+   */
+  async post(client) {
+    const data = {
+      name: this.name,
+      description: this.description,
+      options: this.options
+    }
+    await client.api.applications(client.user.id).commands.post({data: data});
+    return this;
+  }
+
+
+  /**
+   * Deletes command from Discord
+   * @param {Discord.Client} client discord client
+   * @returns {this} this instance
+   */
+  async delete(client) {
+    await client.api.applications(client.user.id).commands(this.id).delete();
+    return this;
+  }
 }
 
 module.exports = GlobalCommand;
