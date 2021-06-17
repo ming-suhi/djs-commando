7e35d33b7127a866d46403afd07718a824679445
明suhi <61369062+ming-suhi@users.noreply.github.com>
2021-06-17 14:08:12 +0800

    Fix class exports
    Add GlobalCommand class and Interaction class to index.js.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-// client
-exports.LocalClient = require('./local-client.js');
+module.exports = {
+  "LocalClient": require('./local-client.js'),
+  "GlobalCommand": require('./structures/global-command.js'),
+  "Interaction": require('./structures/interaction.js'),
+}
